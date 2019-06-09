8eacc50465c1cca692a16574c157f7939290f809
oluwatobi balogun <tobibags19@gmail.com>
2019-06-09 01:11:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {miguel: "girl with the tattoo", party: "break from toronto", frank: "nights", kanye: "saint pablo"}
 
-function updatePlaylist(playlist, artistName, value) {
-  return Object.assign({}, obj, {[String]: value}) 
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, obj, {[artistName]: value}) 
 }
 
 function removeFromPlaylist(obj, string) {
